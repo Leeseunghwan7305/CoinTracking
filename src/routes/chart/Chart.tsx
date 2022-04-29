9e2802b7f73f9a0b2a36566dcd9846aa4b5d2695
Leeseunghwan7305 <sun1238799@naver.com>
2022-04-29 11:30:07 +0900

    [add] apex chart color

--- src/routes/chart/Chart.tsx
+++ src/routes/chart/Chart.tsx
@@ -64,6 +64,18 @@ function Chart() {
               labels: {
                 show: false,
               },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
             },
           }}
         />
@@ -71,5 +83,4 @@ function Chart() {
     </div>
   );
 }
-
 export default Chart;
